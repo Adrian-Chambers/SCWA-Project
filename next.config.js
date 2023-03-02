7cf7ada7890262c998342358dd1540c4e9391575
Adrian-Chambers <47733972+Adrian-Chambers@users.noreply.github.com>
2023-03-02 01:54:48 -0500

    first commit

--- next.config.js
+++
@@ -1,9 +0,0 @@
-// next.config.js
-const isProd = process.env.NODE_ENV === 'production'
-
-module.exports = {
-  assetPrefix: isProd ? '/SCWA-Project/' : '',
-  images: {
-    unoptimized: true,
-  },
-}
